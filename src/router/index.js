4b8cd40c5e37de53c10b7621445949ff72659c63
Williamsxx <111404249+Williamsxx@users.noreply.github.com>
2023-06-26 22:57:22 -0500

    cambiossssssss

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Logeo from '../views/logeo.vue'
+import Main from '../views/Main.vue'
+import Cita from '../views/cita.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'loginn',
+    component: Logeo
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/perfil',
+    name: 'main',
+    component: Main
+  },
+  {
+    path: '/cita',
+    name: 'reservarCita',
+    component: Cita
+  },
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/cita.vue
@@ -0,0 +1,11 @@
+<template name="component-name">
+    <div>
+        <h1>reservarcita</h1>
+    </div>
+</template>
+<script>
+export default{
+    name:"reservarCita"
+}
+
+</script>
